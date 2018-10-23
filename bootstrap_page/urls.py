f9050816e938d525ed6c92ba0f1694c028aa0d8a
DESKTOP-B43K2VI\d0nald <bestwork888@outlook.com>
2018-10-23 15:27:36 -0700

    third commit

--- bootstrap_page/urls.py
+++ bootstrap_page/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'bootstrap_page'
 
 urlpatterns = [
-  path('', views.bootstrap_page.as_view(), name=''),
+  path('', views.bootstrap_page.as_view(), name='contact_list'),
  # path('new', views.BookCreate.as_view(), name='book_new'),
  # path('edit/<int:pk>', views.BookUpdate.as_view(), name='book_edit'),
  # path('delete/<int:pk>', views.BookDelete.as_view(), name='book_delete'),
---
+++ bootstrap_page/views.py
@@ -0,0 +1,4 @@
+from django.http import HttpResponse
+from django.views.generic import TemplateView,ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
