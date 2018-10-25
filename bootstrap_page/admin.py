a97a8e385e906f5053e97d3bbef6821f56c7571c
DESKTOP-B43K2VI\d0nald <bestwork888@outlook.com>
2018-10-25 10:08:55 -0700

    remove dummy code

--- bootstrap_page/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from bootstrap_page.models import ContactList
-
-admin.site.register(ContactList)
--- bootstrap_page/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BootstrapPageConfig(AppConfig):
-    name = 'bootstrap_page'
